1ac080a8efd80eeccc07f3bff980ce9a7775f9d0
Lucas-FernandoJesus <lucasfernandojesus2@gmail.com>
2024-02-03 22:49:37 -0300

    Novo Projeto de Login Criado

--- Site Estatico/js/main.js
+++ Site Estatico/js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();
+$(".slider-principal").slick({
+  dots: true,
+  infinite: true,
+  speed: 900,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
